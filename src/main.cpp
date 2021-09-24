bfcdb33c8dfa49ebff978aacc00af5075b6b7b83
Roy QIU <karoyqiu@gmail.com>
2021-09-24 20:16:36 +0800

    添加空项目。

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+﻿/*! ***********************************************************************************************
+ *
+ * \file        main.cpp
+ * \brief       eyekeeper 主源文件。
+ *
+ * \version     1.0
+ * \date        2021-09-17
+ *
+ * \author      John Doe <johndoe@gmail.com>
+ * \copyright   © 2021 A Company。
+ *
+ **************************************************************************************************/
+#include <iostream>
+
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Keep away from TV!" << std::endl;
+    return 0;
+}
